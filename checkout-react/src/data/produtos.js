05436d63288aad1016bb70f7de045b523d3b6be5
greicibrandt <grebrandt@hotmail.com>
2026-09-13 22:03:59 -0300

    carrinho

---
+++ checkout-react/src/data/produtos.js
@@ -0,0 +1,20 @@
+export const produtos = [
+  {
+    id: 1,
+    nome: "Bandeja com 10 ovos caipiras",
+    preco: 12.90,
+    quantidade: 2,
+  },
+  {
+    id: 2,
+    nome: "Bandeja com 20 ovos caipiras",
+    preco: 23.90,
+    quantidade: 1,
+  },
+  {
+    id: 3,
+    nome: "Bandeja com 30 ovos caipiras",
+    preco: 34.90,
+    quantidade: 1,
+  },
+];
